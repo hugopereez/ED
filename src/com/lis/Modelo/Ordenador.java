fb98d81cac7f32fd6e25de7db6c72d59e3fbbe0d
EstebanMadrazo <spidermanxe@gmail.com>
2020-11-18 00:34:07 -0600

    Update Ordenador.java

--- src/com/lis/Modelo/Ordenador.java
+++ src/com/lis/Modelo/Ordenador.java
@@ -6,7 +6,7 @@ public class Ordenador {
         for (i = 1; i < array.length; i++){
             key = array[i];  
             j = i - 1;  
-            while (j >= 0 && array[j] > key){
+            while (j > 0 && array[j] > key){
           
                 array[j + 1] = array[j];  
                 j = j - 1;  
