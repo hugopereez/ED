383c28359223414325953cf603cf62748caac411
EstebanMadrazo <spidermanxe@gmail.com>
2020-11-18 00:28:23 -0600

    Create Ordenador.java

---
+++ src/com/lis/Modelo/Ordenador.java
@@ -0,0 +1,17 @@
+package com.lis.Modelo;
+
+public class Ordenador {
+    public void insertionSort(int array[]){
+        int i, key, j;  
+        for (i = 1; i < array.length; i++){
+            key = array[i];  
+            j = i - 1;  
+            while (j >= 0 && array[j] > key){
+          
+                array[j + 1] = array[j];  
+                j = j - 1;  
+            }  
+            array[j + 1] = key;  
+        }  
+    }
+}
