42d2d3d54e016a1553446148b22eb2649a434450
urielspv <urielperez9781@gmail.com>
2020-11-25 00:42:12 -0600

    Adaptacion ShellSort

--- src/com/lis/Main.java
+++ src/com/lis/Main.java
@@ -1,8 +1,33 @@
 package com.lis;
 
+import java.io.IOException;
+
+import com.lis.Modelo.ArchivoCSV;
+import com.lis.Modelo.DoubleLinkList;
+import com.lis.Modelo.Ordenador;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+    // write your code here
+    
+
+    ArchivoCSV arch = new ArchivoCSV("src/com/lis/Movie.csv");
+
+    DoubleLinkList db = new DoubleLinkList();
+
+    
+    db = arch.leerCSV();
+
+    db.displayList();
+
+    /* //Prueba de ShellSort (Necesario que las peliculas tengan el parametro 'Duracion')
+    Ordenador orden = new Ordenador();
+    orden.shell(db);
+    System.out.println("inicia lista ordenada por shell -------------------------------------");
+    db.displayList(); */
+
+
+
     }
 }
